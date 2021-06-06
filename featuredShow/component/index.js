b0e1ad1d46ba8429161f62e77c4a3c00e1cddc90
codebender <lionelmbankeu@gmail.com>
2021-06-06 14:51:45 +0200

    we have done it again, will need to check stage mangement for the videos

--- featuredShow/component/index.js
+++ featuredShow/component/index.js
@@ -4,38 +4,65 @@ import { useEffect, useState } from 'components';
 
 export default function FeaturedShow({ featuredVideo }) {
 
+    const [playVideo, isPlay] = useState(false)
+    const [ready, isReady] = useState(false)
+    const [video, setVideo] = useState()
 
 
-    // useEffect(() => {
+    useEffect(() => {
 
-    //     setYoutube(data.genus.filter((i) => i.type == "Youtube"))
+        console.log("featuredVideo", featuredVideo)
+        
+        featuredVideo ? featuredVideo.length > 0 ? setVideo(featuredVideo[0]) : setVideo(false) :setVideo(false)
 
-    //     return () => {
+        return () => {
 
-    //     }
-    // }, [ ])
+        }
+    }, [featuredVideo])
 
 
     return (
-        <div className="w-screen max-w-screen-xl h-full flex flex-col p-2 items-center justify-center relative">
-            <div className="w-full text-start py-2 text-xl">
-                {featuredVideo.items[0].snippet.title}
-            </div>
+
+        video  ?  
+        <div className="w-screen max-w-screen-xl lg:h-full flex flex-col p-2 items-center justify-center relative lg:flex-grow">
+
 
             <div className="w-full flex relative">
-                <img src={`${featuredVideo.items[0].snippet.thumbnails.maxres.url}`} className="w-full h-full z-10"></img>
+                <img src={`${video.items[0].snippet.thumbnails.maxres.url}`} className="w-full h-full z-10"></img>
 
-                <iframe id="player" type="text/html" className="w-full h-full absolute "
-                    src={`http://www.youtube.com/embed/${featuredVideo.items[0].id}?enablejsapi=1`}
+                <div className="w-full h-full absolute z-20 flex items-end">
+                    <div className="w-full h-24 lg:h-32   relative">
+
+                        <div className="w-full h-full bg-black absolute z-20 opacity-50"></div>
+
+                        <div className="z-30 text-start text-xl absolute">
+                            <h1 className="text-white   p-2 ">
+                                {video.items[0].snippet.title}
+                            </h1>
+
+                            <div className="text-white" onClick={() => isPlay(!playVideo)}>
+                                Press Play
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <iframe id="player" type="text/html" className={`w-full h-full absolute ${playVideo ? "z-40" : ""}`}
+                    src={`http://www.youtube.com/embed/${video.items[0].id}?enablejsapi=1`}
                     frameBorder="0"></iframe>
             </div>
 
-            <div className="w-full flex my-2 text-sm lg:text-sm  line-clamp-3">
-                {featuredVideo.items[0].snippet.description ? featuredVideo.items[0].snippet.description : "Air of Mystery"}
+            <div className="w-full flex my-2 text-sm lg:text-sm  lg:line-clamp-3">
+                {video.items[0].snippet.description ? video.items[0].snippet.description : "Air of Mystery"}
             </div>
 
 
-        </div>
+        </div> :
+        <>loading</>
+
 
     )
 }
--- showMenu/component/index.js
+++ showMenu/component/index.js
@@ -1,25 +1,124 @@
 import { useEffect, useState } from 'components';
- 
+import FeaturedShow from 'components/featuredShow/component'
 
 
-export default function ShowMenu({ shows}) {
- 
+export default function ShowMenu({ shows, isReady, setStage }) {
 
 
-    // useEffect(() => {
+    const [categories, setCategories] = useState()
+    const [view, setView] = useState()
+    const [show, setShow] = useState()
 
-    //     setYoutube(data.genus.filter((i) => i.type == "Youtube"))
-         
-    //     return () => {
-             
-    //     }
-    // }, [ ])
+    useEffect(() => {
 
+        console.log("SHow Menu", shows)
+        try {
+
+            if (shows) {
+                let _categories = new Set(shows.map((show) => show.additional))
+
+                setCategories([..._categories])
+            }
+
+        } catch (e) {
+
+            console.error('hello error', e.message)
+        }
+
+
+
+        return () => {
+
+        }
+    }, [])
+
+    useEffect(() => {
+
+        console.log(categories)
+
+        return () => {
+
+        }
+    }, [categories])
+
+
+    const selectVideo = (_show) => {
+
+        console.log(_show)
+        setShow(_show)
+        setView(true)
+        setStage(true)
+
+    }
+
+    const closeStage = () => {
+        setStage(false)
+        setView(false)
+    }
 
     return (
-        <div className="w-screen h-screen bg-white">
 
-        </div>
+        <>
+            {
+
+                shows && isReady ?
+                    <div className="w-screen h-full bg-white">
+
+                        {categories ? categories.map((category) => {
+
+                            return (
+                                <div key={category} className="w-full flex flex-col">
+                                    <div className="p-2">
+                                        <h1>
+                                            {category}
+                                        </h1>
+                                    </div>
+                                    <div className="p-2 flex flex-wrap">
+
+                                        {shows.map((show) => {
+
+                                            if (show.additional == category) {
+                                                return (
+                                                    <a href="#stage" className="w-full lg:w-1/6" onClick={() => selectVideo(show)} key={show.items[0].snippet.title}>
+                                                        <img src={`${show.items[0].snippet.thumbnails.maxres.url}`} className="w-full h-full z-10"></img>
+                                                    </a>
+
+
+                                                )
+                                            }
+
+
+
+
+
+
+                                        })}
+
+                                    </div>
+
+                                </div>
+                            )
+
+                        }) : <></>}
+
+                    </div> : <>loading</>
+
+            }
+
+            {
+                view && show ? <div id="stage" className="w-full h-full absolute flex flex-col bg-white items-center p-4 z-50">
+
+                    <div className="w-full p-2 flex justify-end">
+
+                        <div onClick={() => closeStage()}>close</div>
+
+                    </div>
+
+                    <FeaturedShow featuredVideo={[show]} />
+
+                </div> : <></>
+            }
+        </>
 
     )
 }
